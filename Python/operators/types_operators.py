e5d73d391c9ee9e726306eb049812a32801d2ae7
Maria Suzane <suzaane97@gmail.com>
2024-03-26 10:39:39 -0300

    types operators

---
+++ Python/operators/types_operators.py
@@ -0,0 +1,15 @@
+# parenteses, exponencial, multiplicação, divisão, adição e subtração... nessa ordem.
+
+'''
+== equal
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+'''
+
+# operadores = 10 <= 13
+# print(operadores)
+
+# assignment operators (op. atribuição)
