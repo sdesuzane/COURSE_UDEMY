700c97376701ada25684f36f97e821e2f18b3805
Maria Suzane <suzaane97@gmail.com>
2024-03-28 10:56:33 -0300

    desafios python

---
+++ Python/challenges/functions.py
@@ -0,0 +1,8 @@
+# Calculadora para pintura
+
+'''
+Criar um programa que calcula a quantidadde de tinta necessaria para pintar uma parede. O usuario devera fornecer as seguintes informacoes:
+Rendimento, altura e largura.
+O programa deve mostrar na tela a mensagem "Voce necessita de x latas de tinta"
+
+'''
